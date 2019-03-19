2d31a3138a2014e3a9e6b96ccc02d26952be1c8c
Alexandr Berbat <aleksandrberbat@gmail.com>
2019-03-19 19:08:24 +0200

    some edits
    new structure "EoruContry"
    also added some useful libraries

--- Lab2_Rep/main.cpp
+++ Lab2_Rep/main.cpp
@@ -1,14 +1,31 @@
 #include <iostream>
 #include <conio.h>
 #include <string>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 
+class EuroCountry
+{
+public:
+	string name;
+	int voices;
+};
+
 int main()
 {
+	setlocale(LC_ALL, "Russian");
 
+	int TopMarks[10] = { 12,10,8,7,6,5,4,3,2,1 };
 
+	string path = "eurovision.txt";
 
+	ifstream file;
+
+	file.open(path);
+
+	string line;
 
 	_getch();
 	return 0;
